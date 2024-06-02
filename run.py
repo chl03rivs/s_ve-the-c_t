a16dea0c60b86b74895dd8c1f9fc1e6fd17c8873
Chloë Rivière <chloerivs@gmail.com>
2024-06-02 21:58:41 +0000

    Create game settings class with mode/difficulty options and functions to process the player's choice. Create game_choice function that prompts the user for the game setting choies.

--- run.py
+++ run.py
@@ -25,12 +25,13 @@ alphabet = [(chr(ord(var)+i)) for i in range(26)]
 progress_bar = {
     0: 'intro',
     1: 'rules',
-    2: 'game choice',
-    3: 'guess',
-    4: 'victory',
-    5: 'game over',
-    6: 'replay',
-    7: 'end'
+    2: 'game mode choice',
+    3: 'game diff choice',
+    4: 'guess',
+    5: 'victory',
+    6: 'game over',
+    7: 'replay',
+    8: 'end'
 }
 
 rules = """
@@ -57,6 +58,37 @@ class Player:
         return self.lives_left
         print("Wrong answer! The cat has lost a life @__@ !!")
 
+class GameSettings:
+    """
+    Game mode and difficulty options
+    Functions that will run in game_choice
+    """
+    def __init__(self):
+        self.modes = ['mystery word', 'mystery sentence']
+        self.diffs = ['easy','medium','hard']
+        self.game_mode = ''
+        self.game_diff = ''
+
+    def choose_mode(self, value):
+        if value == '1':
+            self.game_mode = self.modes[0]
+        elif value == '2':
+            self.game_mode = self.modes[1]
+
+        print(f"You have chosen the {self.game_mode} game mode!")
+        return self.game_mode
+    
+    def choose_diff(self, value):
+        if value == '1':
+            self.game_diff = self.diffs[0]
+        elif value == '2':
+            self.game_diff = self.diffs[1]
+        elif value == '3':
+            self.game_diff = self.diffs[2]
+        
+        print(f"You have chosen the {self.game_diff} difficulty!")
+        return self.game_diff
+
 
 def roll_intro():
     """
@@ -74,9 +106,51 @@ def roll_intro():
         if input_validation(skip):
             if skip in answer_yes:
                 print(rules)
+                break
+                print("Loading game...")
             elif skip in answer_no:
                 print("Loading game...")
                 break
+
+
+def game_choice():
+    """
+    Retrieves the game settings via user input
+    Player is prompted to choose mode and difficulty
+    """
+    settings = GameSettings()
+
+    print("Before we begin, you must choose the game mode & difficulty level.\nThe higher the level the more letters you will have to guess before the cat drowns!")
+
+    while True:
+        Player.progress = progress_bar[2]
+        # choice of game mode
+        print("Please choose a game mode by entering 1 or 2 on your keyboard:\nMystery Word [1]\nor\nMystery Sentence [2]")
+
+        p_mode = input('>> ')
+
+        if input_validation(p_mode):
+            game_mode = settings.choose_mode(p_mode)
+            if game_mode:
+                print("Choice saved.")
+                break
+    
+    while True:
+        Player.progress = progress_bar[3]
+        # choice of game difficulty
+        print("Please choose a game difficulty by entering 1, 2 or 3 on your keyboard:\nEasy[1],\nMedium[2],\nHard[3]")
+
+        p_diff = input('>> ')
+
+        if input_validation(p_diff):
+            game_diff = settings.choose_diff(p_diff)
+            if game_diff:
+                print("Choice saved.")
+                break
+
+
+    Player.setting = (settings.game_mode, settings.game_diff)
+    print(f"You are now proceding into\na game with these settings: {Player.setting}")
 
 
 def input_validation(value):
@@ -85,10 +159,9 @@ def input_validation(value):
     Checks user inputs based on where we are in the flowchart
     (using if statements with the progress attribute of the Player class)
     """
-
     # for the yes/no questions
-    if Player.progress in [progress_bar[0], progress_bar[4],
-                           progress_bar[5], progress_bar[6]]:
+    if Player.progress in [progress_bar[0], progress_bar[5],
+                           progress_bar[6], progress_bar[7]]:
         try:
             if value not in answer_yes and value not in answer_no:
                 raise ValueError(f"You must answer yes or no.\nOnly {answer_yes} or {answer_no} are accepted.\nYou entered {value}.")
@@ -112,4 +185,11 @@ def input_validation(value):
     return True
 
 
-roll_intro()
+def main():
+    """
+    Runs the entire application
+    """
+    # roll_intro()
+    game_choice()
+
+main()
